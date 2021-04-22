30f3b71fd2a91cd2fde2131a98edc7518f23e62e
Damian Wąsik <damianwasik98@gmail.com>
2021-04-22 14:25:35 +0200

    Added first example

---
+++ examples/basic_operation.py
@@ -0,0 +1,15 @@
+from dmsales import DMSalesAPI
+
+api = DMSalesAPI(api_key='your-generated-api-key') # you can generate a new key here https://app.dmsales.com/pl/panel/settings-account?settings=api-configuration
+
+# download list of your projects
+projects_list = api.project_list()
+print('MY PROJECTS LIST:\n:', projects_list, '\n')
+
+# download list of your contacts in project
+contacts = api.persons_list(
+    page=1,
+    limit=10,
+    project_id='paste-here-project-id-from-projects-list'
+)
+print('MY CONTACTS:\n', contacts, '\n')
