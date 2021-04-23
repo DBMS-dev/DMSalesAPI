7a632b8893c83b8b893e885621708d0a54d3545b
Damian Wąsik <damianwasik98@gmail.com>
2021-04-23 10:22:04 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dmsales',
+    version='0.1.0',
+    description='DMSales API Python Client',
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.25.1'
+    ]
+)
