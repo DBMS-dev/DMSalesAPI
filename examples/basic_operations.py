798fa4bc7fd60ba930ba7c6a3b623b7cf19be0c2
Damian Wąsik <damianwasik98@gmail.com>
2021-05-19 12:24:19 +0100

    Added user examples

--- examples/basic_operations.py
+++ examples/basic_operations.py
@@ -67,3 +67,11 @@ print(f'Person endpoint response: {response}')
 #update person
 response = api.update_person(id='test_id_1', project_id='paste-here-project-id-from-projects-list', person_dict=person_dict)
 print(f'Person endpoint response: {response}')
+
+#me
+response = api.me()
+print(f'My data: {response}')
+
+#my points
+response = api.my_points()
+print(f'My points: {response}')
