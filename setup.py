01a64fe578fa49446b241a1199bbfec78c1a1dab
Damian Wąsik <damianwasik98@gmail.com>
2021-05-20 13:45:52 +0100

    Fixed bug in custom event endpoint

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dmsales',
-    version='0.1.3',
+    version='0.1.4',
     description='DMSales API Python Client',
     packages=find_packages(),
     install_requires=[
