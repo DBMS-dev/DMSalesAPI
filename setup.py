aea4cc153f1a9781847a3779d197c7227f631892
Damian Wąsik <damianwasik98@gmail.com>
2021-05-21 12:06:43 +0100

    Incremented version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dmsales',
-    version='0.1.4',
+    version='0.1.5',
     description='DMSales API Python Client',
     packages=find_packages(),
     install_requires=[
