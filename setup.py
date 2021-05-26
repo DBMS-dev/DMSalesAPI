05148cf83224d5d5affabc383ff7888227c7c031
Damian Wąsik <53168617+damianwasik98@users.noreply.github.com>
2021-05-26 13:34:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='dmsales',
-    version='0.1.7',
+    version='0.1.8',
     description='DMSales API Python Client',
+    long_description='DMSales API Python Client',
+    url='https://app.dmsales.com/api-doc/default',
+    author='DMSales',
+    author_email='data@dmsales.com',
     packages=find_packages(),
     install_requires=[
         'requests==2.25.1',
