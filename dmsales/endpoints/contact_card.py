1fcff04567ec253885b6396ded75020174aedb5b
szymonfraszak <szymonfraszak@gmail.com>
2021-05-27 12:22:58 +0200

    New endpoint in ContactCardEndpoints class

--- dmsales/endpoints/contact_card.py
+++ dmsales/endpoints/contact_card.py
@@ -5,17 +5,17 @@ from dmsales.api_operations import APIOperations
 logger = logging.getLogger(__name__)
 
 
-class CardEndpoints(APIOperations):
+class ContactCardEndpoints(APIOperations):
 
-    def get_contact_card(self, project_id: str, base_key: str):
+    def contact_card_details(self, project_id: str, base_key: str):
         '''
-        Returns details about record for given base_key.
+        Returns details about contact for given project_id and base_key.
 
         :param project_id: project_id
         :type project_id: str
         :param base_key: base_key
         :type base_key: str
-        :return: Details about record for given base_key
+        :return: details about contact
         :rtype: dict
         '''        
         endpoint = '/api/contact-card/'
@@ -25,5 +25,32 @@ class CardEndpoints(APIOperations):
         }
 
         args_dict = {k: v for k, v in args_dict.items() if v} # exclude None args
-        logger.debug('Calling contact_card method')
-        return super().make_get_request(endpoint, **args_dict)
+        logger.debug('Calling contact_card details method')
+        return super().make_get_request(endpoint, params=args_dict)
+
+    def contact_card_add_note(self, project_id: str, base_key: str, content: str, tag: str):
+        '''
+        Adds note to contact for given project_id and base_key.
+
+        :param project_id: project_id
+        :type project_id: str
+        :param base_key: base_key
+        :type base_key: str
+        :param content: content of the note
+        :type content: str
+        :param tag: tag of the note
+        :type tag: str
+        :return: response for the post request
+        :rtype: str
+        '''        
+        endpoint = '/api/contact-card/add-note'
+        data = {
+            'project_id': project_id,
+            'base_key': base_key,
+            'content': content,
+            'tag': tag
+        }
+
+        data = {k: v for k, v in data.items() if v} # exclude None args
+        logger.debug('Calling add_note method')
+        return super().make_post_request(endpoint, json=data)
